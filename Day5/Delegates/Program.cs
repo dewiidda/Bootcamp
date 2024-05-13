9a70312f0fbcfd93fbccce04b42e207dbc13fd5b
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-13 16:53:19 +0700

    Make a folder and project of what we learn in Day 5 in Bootcamp, include: Delegates, Event, Exception handling and Properties

---
+++ Day5/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿//Method => return type => float
+
+using System.Reflection;
+
+public delegate float MyDelegate(int a, int b);
+
+class Program
+{
+	static void Main()
+	{
+		Subscriber subscriber = new();
+		
+		MyDelegate del= subscriber.Add;
+		del += subscriber.Mul;
+		
+		float result = del(5,5);
+		
+		Delegate[] dell = del.GetInvocationList();
+		foreach(Delegate dg in dell)
+		{
+			Console.WriteLine(dg.GetMethodInfo().Name+":"+((MyDelegate)dg).Invoke(5,5));
+		}
+	}
+}
+
+class Subscriber
+{
+	public float Add(int a, int b)
+	{
+		return (a+b);
+	}
+
+	public float Mul(int a, int b)
+	{
+		return (a*b);
+	}
+}
