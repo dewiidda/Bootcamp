2c50a7ee8cb3c57e7a2c03ce13fc45a6e58d1b71
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-13 16:42:38 +0700

    Make change to Day 2 folder more specific

--- Day2/Composition/Program.cs
+++ Day2/Composition/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main()
     {
         Engine engine = new Engine(1200,"Supra",2023,"Toyota");
-        Vehicle.Wheel wheel = new Vehicle.Wheel("Offroad",40,"Dunlop");
+        Wheel wheel = new Wheel("Offroad",40,"Dunlop");
         Speed speed = new Speed(100.0f);
         Car car = new Car("Black","Toyota",2,engine, wheel, speed);
 
