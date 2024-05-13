3b576c5a2333a7e11001f2d28dfc86eb6ddd7e9e
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-13 16:56:55 +0700

    Make a new folder to b more spesific, Concrete class creation and Constructor

---
+++ Day1/Concrete Class Creation/Program.cs
@@ -0,0 +1,21 @@
+﻿//Cat is a concrete class, because we can intantiate an object of this class use 'new' keyword
+class Program //if there is no access modifier in class, the default is internal
+{
+    static void Main()
+    {
+        Cat cat = new Cat(); //instantiate class 'Cat' into object 'cat'
+        cat.Eat();
+    }
+}
+
+class Cat
+{
+    string color;
+    string species;
+    int age;
+
+    public void Eat()
+    {
+        Console.WriteLine("Cat eat");
+    }
+}
