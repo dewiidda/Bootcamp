9dfd4d77d353ef8ac0b356e101cd21838d1c187f
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-14 15:33:50 +0700

    make a code in project Properties

--- Day5/Properties/Program.cs
+++ Day5/Properties/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Human
+{
+	//Property => PascalCase => Method
+	public int Balance
+	{
+	get {return Balance;} 
+	private set //Bisa melakukan checking sebelum set value
+	{
+		if (value > 0 && value < 1000000)
+		{
+			Balance = value;
+		}else
+		{
+			Console.WriteLine("Maaf, anda tidak boleh ngutang, uang -"); //membuat exception custom
+		};
+	}
+	} 
+	public string Name {get; set;}
+	
+	public void SetBalance(int balance)
+	{
+		Balance = balance;
+	}
+	
+	public void PrintBalance()
+	{
+		Console.WriteLine(Balance);
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Human hm = new();
+		hm.SetBalance(30000000); //memberikan value ke Balance lewat method SetBalance
+	}
+}
