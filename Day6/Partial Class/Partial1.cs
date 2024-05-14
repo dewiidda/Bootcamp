6320a49223d6be478485b56e4b6b64277af23954
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-14 15:14:57 +0700

    make partial class project

---
+++ Day6/Partial Class/Partial1.cs
@@ -0,0 +1,9 @@
+//partial class
+//developer a -> file1.cs
+public partial class Employee
+{
+	public void DoWork()
+	{
+		"Employee working".Dump();
+	}
+}
---
+++ Day6/Partial Class/Partial2.cs
@@ -0,0 +1,9 @@
+//partial class
+//developer b -> file2.cs
+public partial class Employee
+{
+	public void GoLunch()
+	{
+		"Employee go lunch".Dump();
+	}
+}
