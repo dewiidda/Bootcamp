e7b8937733150283879962a7a386dc8472cdb012
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-14 15:20:15 +0700

    make a project nullable value

---
+++ Day6/Nullable value/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+{
+	int? a = null;
+	
+	Nullable<int> nullableInt = null;
+	int? nullableIntt = null;
+	
+	if (a.HasValue)
+	{
+		Console.WriteLine($"a is:{a}");
+	}
+	else
+	{
+		Console.WriteLine("a doesn't have a value");
+	}
+
+}
+}
