6394e3f1469da595a36872b9ad5207cb4d8a5d04
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-14 14:58:53 +0700

    Make a detail code of Enumerator and Iterator in Program.cs

--- Day6/Enumerator and Iterator/Program.cs
+++ Day6/Enumerator and Iterator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Collections.Generic;
+
+void Main()
+{
+	//Enumerator
+	List<string> bp = new (){"Lisa",null,"Jennie","Jisoo","Rose"};
+	IEnumerator<string> enumerator = bp.GetEnumerator();
+	while(enumerator.MoveNext())
+	{
+		Console.WriteLine(enumerator.Current);
+	}
+
+	foreach (int number in GetNumbers())
+	{
+		Console.WriteLine(number);
+	}
+}
+
+//Iterator
+public static IEnumerable<int> GetNumbers()
+{
+	for(int i = 1; i < 100; i++)
+	{
+		yield return i;
+	}
+}
+
+
