2d8be842fa1d1e7e9d557a49f07ffe2fc3e44400
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-14 15:00:03 +0700

    add Class Program

--- Day6/Enumerator and Iterator/Program.cs
+++ Day6/Enumerator and Iterator/Program.cs
@@ -1,29 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-void Main()
+class Program
 {
-	//Enumerator
-	List<string> bp = new (){"Lisa",null,"Jennie","Jisoo","Rose"};
-	IEnumerator<string> enumerator = bp.GetEnumerator();
-	while(enumerator.MoveNext())
-	{
-		Console.WriteLine(enumerator.Current);
-	}
+    void Main()
+    {
+        //Enumerator
+        List<string> bp = new (){"Lisa",null,"Jennie","Jisoo","Rose"};
+        IEnumerator<string> enumerator = bp.GetEnumerator();
+        while(enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
+        }
 
-	foreach (int number in GetNumbers())
-	{
-		Console.WriteLine(number);
-	}
-}
+        foreach (int number in GetNumbers())
+        {
+            Console.WriteLine(number);
+        }
+    }
 
-//Iterator
-public static IEnumerable<int> GetNumbers()
-{
-	for(int i = 1; i < 100; i++)
-	{
-		yield return i;
-	}
+    //Iterator
+    public static IEnumerable<int> GetNumbers()
+    {
+        for(int i = 1; i < 100; i++)
+        {
+            yield return i;
+        }
+    }
+
 }
 
 
