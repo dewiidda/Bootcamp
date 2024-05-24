93da8a69a9697c9c245c8efc6788f88334917820
Dewi Idda R <dewi.rohmana@formulatrix.com>
2024-05-24 15:31:21 +0700

    make finally project

---
+++ Day13/Finally/Program.cs
@@ -0,0 +1,20 @@
+﻿// finally
+class Program{
+    static void Main(){
+        try
+        {
+            for(int i=0; i<100000; i++)
+            {
+                StreamWriter stream = new("../file.txt");
+            }
+        }
+        catch(FormatException e)
+        {
+            Console.WriteLine("Format exception");
+        }
+        finally
+        { // this will be run either there is exception or no
+            Console.WriteLine("Program closed");
+        }
+    }
+}
