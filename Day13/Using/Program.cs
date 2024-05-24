31ffb776f297b99991b8c153a363879e53c87fbc
Dewi Idda R <dewi.rohmana@formulatrix.com>
2024-05-24 15:31:58 +0700

    make using to replace try catch finally for dispose project

---
+++ Day13/Using/Program.cs
@@ -0,0 +1,25 @@
+﻿class Prorgram{
+    static void Main(){
+        // File name
+        string fileName = "../file.txt";
+
+        FileManager fm = new();
+        fm.Write(fileName, "Aku ingin liburan");
+    }
+}
+class FileManager {
+    public void Write(string path, string message) {
+        using (StreamWriter stream = new(path)){
+            stream.WriteLine(message);
+            // stream.Dispose(); // Automatically disposed whether there is exception or not.
+        }
+    }
+    public string ReadLine(string path) {
+        string result;
+        using (StreamReader stream = new(path)){
+            result = stream.ReadLine();
+            // stream.Dispose(); // Automatically disposed when involve "using"
+        }
+        return result;
+    }
+}
