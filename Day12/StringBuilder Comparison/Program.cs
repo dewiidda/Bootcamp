0c8ae6d4925344d17a6983da136b327368a22648
Dewi Idda R <dewi.rohmana@formulatrix.com>
2024-05-24 15:23:23 +0700

    stringBuilder project

--- Day12/StringBuilder Comparison/Program.cs
+++ Day12/StringBuilder Comparison/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+class Program {
+	static void Main() {
+		StringBuilder text = new(); 
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		for (int i = 0; i < 10000000; i++) {
+			text.Append("a");
+			text.Append("b");
+			text.Append("c");
+		}
+		stopwatch.Stop();
+		Console.WriteLine(stopwatch.ElapsedMilliseconds);
+	}
+}
