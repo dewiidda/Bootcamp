99fa64fa31b14fcd584963133129a5372f9df219
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-29 17:05:29 +0700

    add semaphore slim

---
+++ Day15/SemaphoreSlim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+class Program
+{
+    static SemaphoreSlim semaphore = new(2);
+
+    static async Task Main()
+    {
+        Task[] tasks = new Task[10];
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            int index = i;
+            tasks[i] = Task.Run(async () => await DoWork(index));
+        }
+        await Task.WhenAll();
+    }
+
+    static async Task DoWork(int index)
+    {
+        Console.WriteLine($"Task {index} started");
+
+        await semaphore.WaitAsync();
+        await Task.Delay(500);
+        Console.WriteLine($"Task {index} processing");
+        await Task.Delay(500);
+        semaphore.Release();
+
+        Console.WriteLine($"Task {index} finished");
+    }
+}
