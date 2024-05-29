6867f9596f3dc329e9e87a85f41454c17f3f76c0
Dewi Idda <dewi.rohmana@formulatrix.com>
2024-05-29 17:12:41 +0700

    make a project for day 16 about data contract, filestream,stream writer and data serialization and deserialization (JSON and XML)

---
+++ Day16/FileStream/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (FileStream fs = new FileStream("test1.txt", FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+            string text = "Selamat pagi, saya agak lapar";
+            byte[] myBytes = new byte[text.Length]; //buffer
+
+            myBytes = Encoding.UTF8.GetBytes(text);
+            fs.Write(myBytes, 0, myBytes.Length);
+            Console.ReadLine();
+        }
+
+        Console.ReadLine();
+        Console.WriteLine();
+
+        using (FileStream fs1 = File.OpenRead("test1.txt"))
+        {
+            byte[] b = new byte[1024];
+            UTF8Encoding tmp = new UTF8Encoding(true);
+            int readLen;
+            while ((readLen = fs1.Read(b, 1, b.Length)) > 0)
+            {
+                Console.WriteLine(tmp.GetString(b, 0, readLen));
+            }
+        }
+
+        using (var sr = new StreamReader("test1.txt"))
+        {
+            Console.WriteLine(sr.ReadToEnd());
+        }
+
+    }
+}
---
+++ Day16/StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        using (FileStream fs = new("file.txt", FileMode.Create, FileAccess.Write))
+        {
+            string[] input = { "Saya adalah manusia", "Yang hidup di dunia", "Fana ini" };
+            using (StreamWriter outputFile = new StreamWriter(fs))
+            {
+                foreach (var word in input)
+                {
+                    outputFile.WriteLine(word);
+                }
+            }
+        }
+
+        using (FileStream fs = new("file.txt", FileMode.Append, FileAccess.Write))
+        {
+            using (StreamWriter outputFile = new(fs))
+            {
+                outputFile.WriteLine("Tapi saya suka makan bakso");
+            }
+        }
+
+        using (StreamReader inputFile = new("file.txt"))
+        {
+            Console.WriteLine(inputFile.ReadLine());
+            Console.WriteLine(inputFile.ReadLine());
+            Console.WriteLine(inputFile.ReadLine());
+            Console.WriteLine(inputFile.ReadLine());
+        }
+    }
+}
